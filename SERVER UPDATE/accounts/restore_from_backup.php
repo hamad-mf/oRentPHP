dfe3f0ea8aabb37480b0a3f95aae35f5f052a972
hamad-mf <hamadmf100@gmail.com>
2026-03-23 23:10:54 +0530

    Removed unwanted folders for light version

--- SERVER UPDATE/accounts/restore_from_backup.php
+++
@@ -1,6 +0,0 @@
-<?php
-copy(
-    __DIR__ . '/SERVER UPDATE/accounts/index.php',
-    __DIR__ . '/accounts/index.php'
-);
-echo "Restored!\n";
--- SERVER UPDATE/leads/index.php
+++
@@ -1,3 +0,0 @@
-<?php
-require_once __DIR__ . '/../config/db.php';
-redirect('pipeline.php');
